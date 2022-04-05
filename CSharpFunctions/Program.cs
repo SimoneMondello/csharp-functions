fc73fce4537f0856f6ae536cbc875e93ad306d63
SimoneMondello <mondellosimo@gmail.com>
2022-04-05 17:18:28 +0200

    commit.1

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,1 +1,80 @@
 ﻿
+/* 
+ * Scrivete nel vostro programma principale Program.cs le seguenti funzioni di base:
+- void StampaArray(int[] array): che preso un array di numeri interi, stampa a video il contenuto dell’array 
+in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. 
+Potete prendere quella fatta in classe questa mattina 
+
+- int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+
+- int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi,
+restituisca un nuovo array con tutti gli elementi elevati quadrato.
+Attenzione: è importante restituire un nuovo array, 
+e non modificare l’array come parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
+
+- int sommaElementiArray(int[] array): che preso un array di numeri interi,
+restituisca la somma totale di tutti gli elementi dell’array.
+
+Una volta completate queste funzioni di utilità di base, 
+e dato il seguente array di numeri [2, 6, 7, 5, 3, 9] già dichiarato nel vostro codice, si vogliono utilizzare le funzioni per:
+Stampare l’array di numeri fornito a video
+Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato (Verificare che l’array originale non sia stato modificato, ristampo nuovamente l’array originale e verificando che sia rimasto [2, 6, 7, 5, 3, 9])
+Stampare la somma di tutti i numeri
+Stampare la somma di tutti i numeri elevati al quadrati */
+
+
+//Prima funzione
+void StampaArrayInteri(int[] array)
+{
+    int ultimoIndiceArray = array.Length - 1;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == ultimoIndiceArray)
+        {
+            Console.Write(array[i]);
+        }
+        else
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] arrayDiNumeri = { 2, 6, 7, 5, 3, 9 };
+StampaArrayInteri (arrayDiNumeri);
+
+
+
+
+
+/* Stampo al quadrato ogni singolo numero */
+
+
+
+for (int i = 0; i < arrayDiNumeri.Length; i++)
+
+ {
+    double quadrato = Math.Pow (arrayDiNumeri[i], 2);
+    Console.WriteLine (quadrato + "numero al quadrato");
+
+   
+}
+
+
+
+
+
+//Somma dei numeri
+
+
+
+    int[] array = arrayDiNumeri;
+
+    int sum = 0;
+    Array.ForEach(array, i => sum += i);
+
+    Console.WriteLine(sum + " totale");
+
+
